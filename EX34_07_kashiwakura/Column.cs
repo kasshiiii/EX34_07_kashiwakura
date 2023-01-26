d24b8c9b5ff7bf0db7bff177d3241eb52ed9a7f3
kasshiiii <109583121+kasshiiii@users.noreply.github.com>
2023-01-26 15:16:27 +0900

    プロジェクト ファイルを追加します。

---
+++ EX34_07_kashiwakura/Column.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeSolid
+{
+    internal class Column : ISolid
+    {
+        protected float height;
+        protected IShape shape;
+        public Column(IShape shape, float height)
+        {
+            this.shape = shape;
+            this.height = height;
+        }
+        public float Volume
+        {
+            get=>shape.Surface * this.height;
+        }
+        public float Surface
+        {
+            get=>shape.Surface * 2 + shape.Circumference * this.height;
+        }
+    }
+    internal class Column<T> : ISolid where T : IShape
+    {
+        protected float height;
+        protected T shape;
+        public Column(T shape, float height)
+        {
+            this.shape = shape;
+            this.height = height;
+        }
+        public float Volume
+        {
+            get => shape.Surface * this.height;
+        }
+        public float Surface
+        {
+            get => shape.Surface * 2 + shape.Circumference * this.height;
+        }
+    }
+}
